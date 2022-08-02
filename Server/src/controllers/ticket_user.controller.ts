2306fb6c12c931aceb1f91bf416f24ad48c894fd
ZEE <supachok768@gmail.com>
2022-08-03 01:14:17 +0700

    add socket io and change req params type

--- Server/src/controllers/ticket_user.controller.ts
+++ Server/src/controllers/ticket_user.controller.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { TicketUser } from '@prisma/client';
 import TicketUserService from '@/services/tickets_user.service';
+import { RequestWithUser } from '@/interfaces/auth.interface';
 class TicketUserController {
   public ticketUserService = new TicketUserService();
 
-  public getTicketUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  public getTicketUser = async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const userId = Number(req.user['id']);
+      const userId = Number(req.user.id);
       const findAllTicketUserData = await this.ticketUserService.findTicketUser(userId,Number(req.params.perPage), Number(req.params.numPage));
 
       res.status(200).json({ data: findAllTicketUserData, message: 'findAll' });
